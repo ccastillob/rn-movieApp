8101085ae1bf40b1f946c688ec81dd0622282813
ccastillob <ccastillo993a@gmail.com>
2024-05-04 19:23:16 -0500

    feat: add data movie by id

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,6 +1,16 @@
+import { useRoute } from '@react-navigation/native';
 import { Text, View } from 'react-native';
+import { RootStackParams } from '../../navigation/Navigation';
+import { StackScreenProps } from '@react-navigation/stack';
 
-export const DetailsScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'Details'>{};
+
+export const DetailsScreen = ({route}: Props) => {
+
+  const { movieId } = route.params;
+
+  console.log({movieId})
+
   return (
     <View>
       <Text>DetailsScreen</Text>
